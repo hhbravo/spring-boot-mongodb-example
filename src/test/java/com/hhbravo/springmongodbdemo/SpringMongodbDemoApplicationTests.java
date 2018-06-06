b7cfa2f980e6181f6fc559567870eea3eaac06e9
hans hb <hansfranklin.herrera@entelgy.com>
2018-06-05 23:27:09 -0500

    primer commit

---
+++ src/test/java/com/hhbravo/springmongodbdemo/SpringMongodbDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hhbravo.springmongodbdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMongodbDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
